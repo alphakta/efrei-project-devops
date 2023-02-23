bad3d0cdaa58f1546888748c259e0f749b1f4664
unknown <alpha.keita@efrei.net>
2023-02-23 22:25:56 +0100

    test

--- vues/index_site.php
+++ vues/index_site.php
@@ -40,7 +40,7 @@
 
 
 		<div class="well well-lg"> 
-			<h1 class="text-center"> test bro</h1> 
+			<h1 class="text-center"> test Be -PRIMEUR </h1> 
 			<p class= "text-center"> 
 			Avec Be-Primeur, vos fruits et légumes cueillis le matin et livrés le lendemain !
 			En matière de goût, le chemin le plus court est toujours le meilleur… </br>
