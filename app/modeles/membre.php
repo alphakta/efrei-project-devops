503a08f82957d5e6d200d583300fe93a29ab6a97
unknown <alpha.keita@efrei.net>
2023-03-12 01:02:17 +0100

    update files on controleurs

--- app/modeles/membre.php
+++ app/modeles/membre.php
@@ -40,12 +40,11 @@
     	}
     }
   
-    public function ajoutBD($co, $prenom, $nom, $mdp, $email, $adresse, $tel, $codepostal, $ville) {
+    public function ajoutBD($prenom, $nom, $mdp, $email, $adresse, $tel, $numQuartier) {
         //mysqli_query($co, "INSERT into quartier (codeQuartier, nomQuartier) VALUES ('$codepostal','$ville')");
-        $numQuartier = mysqli_insert_id($co);
         $mdpHash = password_hash($mdp, PASSWORD_DEFAULT);
 
-    	mysqli_query($co, "insert into client (prenomClient, nomClient, mdpClient, mailClient, adresseClient, telClient, numQuartier) VALUES ('$prenom','$nom','$mdpHash','$email','$adresse','$tel', '$numQuartier')");
+    	mysqli_query($this->co, "insert into client (prenomClient, nomClient, mdpClient, mailClient, adresseClient, telClient, numQuartier) VALUES ('$prenom','$nom','$mdpHash','$email','$adresse','$tel', '$numQuartier')");
     }
 
     public function initialiser($mail, $mdp)
